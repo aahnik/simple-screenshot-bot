34c3281965325ec1a77110c3cb32ab854549c21b
aahnik <66209958+aahnik@users.noreply.github.com>
2021-01-20 18:36:56 +0530

    launch browser only once

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ logging.basicConfig(level=logging.INFO)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
+browser, page = None, None
+
+
+async def start_browser():
+    global browser, page
+    browser = await launch(headless=True)
+    page = await browser.newPage()
+
+
 @bot.on(events.NewMessage(pattern='/start'))
 async def start(event):
     await event.respond('Hi! I am alive. \
@@ -19,9 +28,6 @@ async def start(event):
 
 @bot.on(events.NewMessage)
 async def echo(event):
-    browser = await launch()
-    page = await browser.newPage()
-
     try:
         urls = fetch_urls(event.text)
         for url in urls:
@@ -29,8 +35,8 @@ async def echo(event):
             await page.goto(url)
 
             file_name = f'{time.time()}.png'
-            await page.screenshot(path=file_name,fullPage=False)
-            await event.reply(event.text,file=file_name)
+            await page.screenshot(path=file_name, fullPage=False)
+            await event.reply(event.text, file=file_name)
             os.remove(file_name)
 
     except Exception as err:
@@ -38,11 +44,10 @@ async def echo(event):
         await event.respond(str(err)[:2000])
         logging.exception(err)
         return
-    await browser.close()
-
-
-
 
 
 if __name__ == '__main__':
-    bot.run_until_disconnected()
+
+    with bot:
+        bot.loop.run_until_complete(start_browser())
+        bot.run_until_disconnected()
