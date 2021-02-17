04d0862c135b7ecf6174a0aff5ae395697b06db1
Abhijith NT <abhijithnt64@gmail.com>
2021-02-17 22:49:01 +0530

    Update

--- utils.py
+++ utils.py
@@ -1,7 +1,4 @@
-import re
-
-def fetch_urls(string):
+class Re:
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    url = re.findall(regex,string)
-    return [x[0] for x in url]
     
+        
